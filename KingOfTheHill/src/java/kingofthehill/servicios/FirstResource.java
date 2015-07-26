27b66c9cec664570ec8014026219c906bee4a2dc
Santiago-vdk <Santiago.vdk@gmail.com>
2015-07-26 14:59:18 -0600

    Commit INCIAL, se añade servicio de ejemplo

---
+++ KingOfTheHill/src/java/kingofthehill/servicios/FirstResource.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kingofthehill.servicios;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+@Path("First")
+public class FirstResource {
+    @GET
+    @Produces("text/plain")
+    public String getText() {
+        return "My First RESTful Servivce....";
+    }
+}
---
+++ KingOfTheHill/web/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : Jul 26, 2015, 2:36:44 PM
+    Author     : Shagy
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
