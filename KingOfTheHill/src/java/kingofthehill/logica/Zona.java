ac60279af551d3e746c160e39ea89678258aa940
Rafael Chacón Fallas <rafangel1228@gmail.com>
2015-08-06 20:18:05 -0600

    se agregaron atributos faltantes

--- KingOfTheHill/src/java/kingofthehill/logica/Zona.java
+++ KingOfTheHill/src/java/kingofthehill/logica/Zona.java
@@ -13,9 +13,17 @@ public class Zona {
     private Zona _next = null;
     private Zona _prev = null;
     private String _escuela = "";
+    private String _lat1 ="";
+    private String _long1 = "";
+    private String _lat2 ="";
+    private String _long2 = "";
     
-    public Zona(String pEscuela){
+    public Zona(String pEscuela,String pLat1,String pLong1,String pLat2,String pLong2){
         _escuela = pEscuela;
+        _lat1 = pLat1;
+        _long1 = pLong1;
+        _lat2 = pLat2;
+        _long2 = pLong2;
     }
 
     /**
@@ -58,6 +66,62 @@ public class Zona {
      */
     public void setEscuela(String pescuela) {
         _escuela = pescuela;
+    }
+
+    /**
+     * @return the _lat1
+     */
+    public String getLat1() {
+        return _lat1;
+    }
+
+    /**
+     * @param _lat1 the _lat1 to set
+     */
+    public void setLat1(String _lat1) {
+        this._lat1 = _lat1;
+    }
+
+    /**
+     * @return the _long1
+     */
+    public String getLong1() {
+        return _long1;
+    }
+
+    /**
+     * @param _long1 the _long1 to set
+     */
+    public void setLong1(String _long1) {
+        this._long1 = _long1;
+    }
+
+    /**
+     * @return the _lat2
+     */
+    public String getLat2() {
+        return _lat2;
+    }
+
+    /**
+     * @param _lat2 the _lat2 to set
+     */
+    public void setLat2(String _lat2) {
+        this._lat2 = _lat2;
+    }
+
+    /**
+     * @return the _long2
+     */
+    public String getLong2() {
+        return _long2;
+    }
+
+    /**
+     * @param _long2 the _long2 to set
+     */
+    public void setLong2(String _long2) {
+        this._long2 = _long2;
     }
     
     
