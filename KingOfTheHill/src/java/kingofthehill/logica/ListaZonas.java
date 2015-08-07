1c44318fa01537017a75b71bc66f22d535fb019d
Rafael Chacón Fallas <rafangel1228@gmail.com>
2015-08-06 19:55:17 -0600

    actualizacion de metodos

--- KingOfTheHill/src/java/kingofthehill/logica/ListaZonas.java
+++ KingOfTheHill/src/java/kingofthehill/logica/ListaZonas.java
@@ -13,6 +13,65 @@ public class ListaZonas {
     private Zona _head = null;
     private Zona _tail = null;
     private int _tam = 0;
+    
+    
+    
+    
+    public void insertar(String pEscuela){
+        Zona tmp = new Zona(pEscuela);
+        if(_head == null){
+            _head = tmp;
+            _tail = tmp;
+        }
+        else{
+            _tail.setNext(tmp);
+            tmp.setPrev(_tail);
+            _tail = tmp;
+        }
+        _tam++;
+    }
+    
+    public Zona buscar(int pIndice){
+        Zona tmp = _head;
+        for(int i=0;i<_tam && i<pIndice;i++){
+            tmp = tmp.getNext();
+        }
+        return tmp;
+    }
+    
+    public void borrar(int pIndice){
+        if(pIndice < _tam){
+            Zona tmp = _head;
+            for(int i=0;i<_tam && i<pIndice;i++){
+                tmp = tmp.getNext();
+            }
+            if(_tam ==1){
+                    _head = null;
+                    _tail = null;
+                }
+                
+                //se valida si es el primer elemento
+                else if(tmp == _head){
+                    tmp.getNext().setPrev(null);
+                    _head = tmp.getNext();
+                    _tam--;
+                }
+                
+                //se valida si es el ultimo elemento
+                else if(tmp == _tail){
+                    tmp.getPrev().setNext(null);
+                    _tail = tmp.getPrev();
+                    _tam--;
+                }
+                else{
+                    tmp.getPrev().setNext(tmp.getNext());
+                    tmp.getNext().setPrev(tmp.getPrev());
+                    _tam--;
+                }
+        }
+    }
+    
+    
 
     /**
      * @return the _head
